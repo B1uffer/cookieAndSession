90f2ef4c1b480036c0c98b67eef96c48421267d8
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-06 03:24:36 +0900

    세션 관리 핵심 컴포넌트 placeOrder 메서드 구현

--- src/main/java/com/b1uffer/cookieandsession/security/service/OrderService.java
+++ src/main/java/com/b1uffer/cookieandsession/security/service/OrderService.java
@@ -1,10 +1,15 @@
 package com.b1uffer.cookieandsession.security.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
-    public void placeOrder() {
 
+    public void placeOrder() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        System.out.println(username + "님의 주문이 접수되었습니다.");
     }
 }
