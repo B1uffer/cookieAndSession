0a883785ee3086a3226c7fa135b43af247475bdf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-06 03:22:24 +0900

    세션 관리 핵심 컴포넌트 UserController me API 구현

--- src/main/java/com/b1uffer/cookieandsession/security/controller/UserController.java
+++ src/main/java/com/b1uffer/cookieandsession/security/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.b1uffer.cookieandsession.security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @GetMapping("/me")
+    public String me() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "현재 사용자는 " + authentication.getName() + "이고, 권한은 " + authentication.getAuthorities();
+    }
 }
