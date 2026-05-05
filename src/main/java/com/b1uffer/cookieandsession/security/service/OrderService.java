c0471971ee8b5f35059612a44c365c9357b310d3
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-06 03:23:14 +0900

    세션 관리 핵심 컴포넌트 placeOrder 메서드 생성

--- src/main/java/com/b1uffer/cookieandsession/security/service/OrderService.java
+++ src/main/java/com/b1uffer/cookieandsession/security/service/OrderService.java
@@ -1,4 +1,10 @@
 package com.b1uffer.cookieandsession.security.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+    public void placeOrder() {
+
+    }
 }
