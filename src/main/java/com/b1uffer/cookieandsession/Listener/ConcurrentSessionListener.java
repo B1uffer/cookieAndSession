605af54876015b15d01c1b310401f435ffec5693
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-07 15:51:11 +0900

    세션 관리 핵심 컴포넌트 세션 이벤트, 리스너 ConcurrentSessionListener 구현

--- src/main/java/com/b1uffer/cookieandsession/Listener/ConcurrentSessionListener.java
+++ src/main/java/com/b1uffer/cookieandsession/Listener/ConcurrentSessionListener.java
@@ -1,4 +1,17 @@
 package com.b1uffer.cookieandsession.Listener;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.security.web.session.HttpSessionDestroyedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ConcurrentSessionListener {
+    @EventListener
+    public void onSessionDestroyed(HttpSessionDestroyedEvent event) {
+        String sessionId = event.getSession().getId();
+        System.out.println("[ConcurrentSessionFilter] 세션 만료 감지 - ID : " + sessionId);
+        event.getSecurityContexts().forEach(context -> {
+            System.out.println("만료된 사용자 : " + context.getAuthentication().getName());
+        });
+    }
 }
