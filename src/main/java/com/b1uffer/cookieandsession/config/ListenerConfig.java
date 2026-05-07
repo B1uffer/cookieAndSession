bb463d0ca764abc1cf21da4a493de0141a0177ac
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-07 14:54:59 +0900

    세션 관리 핵심 컴포넌트 세션 이벤트, 리스너 ListenerConfig 구현

--- src/main/java/com/b1uffer/cookieandsession/config/ListenerConfig.java
+++ src/main/java/com/b1uffer/cookieandsession/config/ListenerConfig.java
@@ -1,4 +1,15 @@
 package com.b1uffer.cookieandsession.config;
 
+import com.b1uffer.cookieandsession.Listener.CustomHttpSessionListener;
+import jakarta.servlet.http.HttpSessionListener;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ListenerConfig {
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionListener> httpSessionListener() {
+        return new ServletListenerRegistrationBean<>(new CustomHttpSessionListener());
+    }
 }
