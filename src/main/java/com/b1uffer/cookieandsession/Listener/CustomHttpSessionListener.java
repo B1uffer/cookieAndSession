e4784901fd0ce7f28533603ac76d860141b4fb2f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-07 14:49:21 +0900

    세션 관리 핵심 컴포넌트 세션 이벤트, 리스너 CustomHttpSessionListener 구현

--- src/main/java/com/b1uffer/cookieandsession/Listener/CustomHttpSessionListener.java
+++ src/main/java/com/b1uffer/cookieandsession/Listener/CustomHttpSessionListener.java
@@ -1,4 +1,22 @@
 package com.b1uffer.cookieandsession.Listener;
 
-public class CustomHttpSessionListener {
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomHttpSessionListener implements HttpSessionListener {
+    private static final Logger log = LoggerFactory.getLogger(CustomHttpSessionListener.class);
+
+    // 세션의 생성
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        log.info("[세션 생성] sessionId = {} 생성됨", se.getSession().getId());
+    }
+
+    // 세션의 소멸
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        log.info("[세션 소멸] sessionId = {} 소멸됨", se.getSession().getId());
+    }
 }
