ca8a3f49e4a4754eaca20c73e5d169d11532e735
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-07 15:26:04 +0900

    세션 관리 핵심 컴포넌트 세션 이벤트, 리스너 SessionDestroyedEventListener 구현

--- src/main/java/com/b1uffer/cookieandsession/Listener/SessionDestroyedEventListener.java
+++ src/main/java/com/b1uffer/cookieandsession/Listener/SessionDestroyedEventListener.java
@@ -1,4 +1,16 @@
 package com.b1uffer.cookieandsession.Listener;
 
-public class SessionDestroyedEventListener {
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.web.session.HttpSessionDestroyedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionDestroyedEventListener implements ApplicationListener<HttpSessionDestroyedEvent> {
+    @Override
+    public void onApplicationEvent(HttpSessionDestroyedEvent event) {
+        event.getSecurityContexts().forEach(content -> {
+            String username = content.getAuthentication().getName();
+            System.out.println("[세션 만료 감지] 사용자 : " + username);
+        });
+    }
 }
