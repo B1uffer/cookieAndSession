2319149d1e6fd04b6fd0dbefd9ccb5dd8f432b4b
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-10 15:05:23 +0900

    세션 관리 설정과 커스터마이징 세션 만료 후 처리 전략 공백 정리

--- src/main/java/com/b1uffer/cookieandsession/security/custom/CustomInvalidSessionStrategy.java
+++ src/main/java/com/b1uffer/cookieandsession/security/custom/CustomInvalidSessionStrategy.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.web.session.InvalidSessionStrategy;
 import org.springframework.stereotype.Component;
 
-
 import java.io.IOException;
 
 @Component
