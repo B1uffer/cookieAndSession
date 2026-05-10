fa97bb3ee3ea88f640c69540fc2aa28f2cfbdd39
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-10 15:04:21 +0900

    세션 관리 설정과 커스터마이징 세션 만료 후 처리 전략  CustomInvalidSessionStrategy API 구현

--- src/main/java/com/b1uffer/cookieandsession/security/custom/CustomInvalidSessionStrategy.java
+++ src/main/java/com/b1uffer/cookieandsession/security/custom/CustomInvalidSessionStrategy.java
@@ -1,4 +1,25 @@
 package com.b1uffer.cookieandsession.security.custom;
 
-public class CustomInvalidSessionStrategy {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.session.InvalidSessionStrategy;
+import org.springframework.stereotype.Component;
+
+
+import java.io.IOException;
+
+@Component
+public class CustomInvalidSessionStrategy implements InvalidSessionStrategy {
+
+
+    @Override
+    public void onInvalidSessionDetected(HttpServletRequest request,
+                                         HttpServletResponse response) throws IOException, ServletException {
+        // REST API 응답 예
+        response.setContentType("application/json; charset=UTF-8"); // 응답 타입 정해주기
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+        response.getWriter().write("{\"error\": \"세션이 만료되었습니다. 다시 로그인해주세요.\"}");
+        response.getWriter().flush(); // flush로 한번에 보내기
+    }
 }
