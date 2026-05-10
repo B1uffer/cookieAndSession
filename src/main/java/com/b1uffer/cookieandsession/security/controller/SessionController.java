d2a5394157a8ffa48b1e076d741d08b4676eebbf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-10 14:41:23 +0900

    세션 관리 핵심 컴포넌트 세션 만료 후 처리 전략 SessionController API 구현

--- src/main/java/com/b1uffer/cookieandsession/security/controller/SessionController.java
+++ src/main/java/com/b1uffer/cookieandsession/security/controller/SessionController.java
@@ -1,4 +1,14 @@
 package com.b1uffer.cookieandsession.security.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SessionController {
+    @GetMapping("/session-expired")
+    public String sessionExpired(Model model) {
+        model.addAttribute("msg", "세션이 만료되었습니다. 다시 로그인해주세요");
+        return "session-expired";
+    }
 }
